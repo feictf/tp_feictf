bad0e6c9811e9adfe0de26cb8077d8c40a52436e
root <root@guest.guest>
2022-05-15 07:56:42 +0000

    zmeny na uvodnej stranke

--- dojo_plugin/api/v1/download.py
+++ dojo_plugin/api/v1/download.py
@@ -1,16 +1,23 @@
 from flask import url_for
-from flask import send_from_directory
+from flask import send_from_directory, current_app
 from flask_restx import Namespace, Resource
-from ...utils import CHALLENGES_DIR
+from CTFd.utils import get_app_config
+# from ...utils import CHALLENGES_DIR
+import pathlib
+import os
 from os import path
+
+
+CHALLENGES_DIR = pathlib.Path("/var/challenges")
+
 
 download_namespace = Namespace("download")
 
-
-@download_namespace.route("/<path:category>/<path:filename>")
+@download_namespace.route("/<path:filename>")
 class downloadBinary(Resource):
-    def get(self, category, filename):
-        tmp = category + "/" + filename + "/" + filename
-        # return send_from_directory("opt/dojo/data/challenges/" + tmp, filename)
+    def get(self, filename):
+        #tmp = "/" + category + "/" + filename + "/" + filename
+        uploads = os.path.join(current_app.root_path, get_app_config('UPLOAD_FOLDER'))
+        return send_from_directory(directory=uploads, filename=filename)
         return path.exists("logs.txt")
-    
+
