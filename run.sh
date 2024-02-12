68c98be8145df51e8d0e03295882ad3a8b9d861f
root <71690048+palowashere@users.noreply.github.com>
2024-02-12 07:43:26 +0000

    2024

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "ahoj kubo -> cau palo"
 echo $(date); echo "Run spusteny! od" $(tty) | wall
 
 DIR="$(readlink -f $(dirname $0))"
@@ -26,8 +25,8 @@ docker run \
        --name pwn.college \
        pwn.college
 
-#docker exec pwn.college bash
-docker exec -it pwn.college bash -c "sleep 18; docker exec -it ctfd_proxy bash -c 'sed -i \"53,59d\" /etc/nginx/conf.d/default.conf; sleep 4; service nginx restart; sed -i \"58d\" /etc/nginx/conf.d/default.conf; service nginx restart;'"
+docker exec pwn.college bash -c "sleep 10; docker compose up"
+#docker exec -it pwn.college bash -c "sleep 18; docker exec -it ctfd_proxy bash -c 'sed -i \"53,59d\" /etc/nginx/conf.d/default.conf; sleep 4; service nginx restart; sed -i \"58d\" /etc/nginx/conf.d/default.conf; service nginx restart;'"
 
 
 echo "Run done." | wall
